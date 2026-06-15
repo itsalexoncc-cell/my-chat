2cea261e91ed2f4730691ef6baad718107a3c278
Alexander <itsalexoncc@gmail.com>
2026-06-15 21:50:46 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,7 +4,6 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
   browserLocalPersistence,
-  setPersistence,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
